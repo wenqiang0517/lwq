ce224e9ce7dcac1751dbbc519d6960574dcd7ea6
janvee <[2899632579@qq.com]>
2019-05-21 15:24:22 +0800

    test3 add

---
+++ src/main/java/org/lwq/servlet/Test2Servlet.java
@@ -0,0 +1,28 @@
+package org.lwq.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description
+ * @Author janvee
+ * @Date: 2019/5/21 14:26
+ **/
+@WebServlet("/test3")
+public class Test2Servlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("hello world,test3!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
