2af124bec92be340c748386af3adf5f650aefbf9
janvee <[2899632579@qq.com]>
2019-05-21 14:23:50 +0800

    first2

---
+++ src/main/java/org/lwq/Test.java
@@ -0,0 +1,13 @@
+package org.lwq;
+
+/**
+ * @Description
+ * @Author lwq
+ * @Date: 2019/5/21 14:23
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("lwq");
+    }
+}
